80003f9da7b7309ea9be1f5f75cecf1cf85f7746
SerjoMhana <serjomh33@gmail.com>
2024-09-03 07:23:55 +0200

    some changes

--- library_management_system/library_management_system/doctype/library_member/library_member.py
+++ library_management_system/library_management_system/doctype/library_member/library_member.py
@@ -6,4 +6,7 @@ from frappe.model.document import Document
 
 
 class LibraryMember(Document):
-	pass
+    #this method will run every time a document is saved
+    def before_save(self):
+        self.full_name = f'{self.first_name} {self.last_name or ""}'
+        pass
--- library_management_system/library_management_system/doctype/library_membership/library_membership.py
+++ library_management_system/library_management_system/doctype/library_membership/library_membership.py
@@ -1,9 +1,60 @@
-# Copyright (c) 2024, seraj and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
+class LibraryTransaction(Document):
+    def before_submit(self):
+        if self.transaction_type == "Issue":
+            self.validate_issue()
+            self.validate_maximum_limit()
+            # Set the article status to Inactive (since it's issued)
+            article = frappe.get_doc("Article", self.article)
+            article.status = "Inactive"
+            article.save()
+            frappe.db.commit()  # Ensure the status change is committed to the database
+            frappe.log_error(f"Article {article.name} status changed to Inactive", "Library Transaction")
 
-class LibraryMembership(Document):
-	pass
+        elif self.transaction_type == "Return":
+            self.validate_return()
+            # Set the article status to Active (since it's returned)
+            article = frappe.get_doc("Article", self.article)
+            article.status = "Active"
+            article.save()
+            frappe.db.commit()  # Ensure the status change is committed to the database
+            frappe.log_error(f"Article {article.name} status changed to Active", "Library Transaction")
+
+    def validate_issue(self):
+        self.validate_membership()
+        article = frappe.get_doc("Article", self.article)
+        # Article cannot be issued if it is already inactive (issued)
+        if article.status == "Inactive":
+            frappe.throw("Article is already issued by another member")
+
+    def validate_return(self):
+        article = frappe.get_doc("Article", self.article)
+        # Article cannot be returned if it is Active (not issued)
+        if article.status == "Active":
+            frappe.throw("Article cannot be returned without being issued first")
+
+    def validate_maximum_limit(self):
+        max_articles = frappe.db.get_single_value("Library Management Settings", "max_articles")
+        count = frappe.db.count(
+            "Library Transaction",
+            {"library_member": self.library_member, "transaction_type": "Issue", "docstatus": DocStatus.submitted()},
+        )
+        if count >= max_articles:
+            frappe.throw("Maximum limit reached for issuing articles")
+
+    def validate_membership(self):
+        # Check if a valid membership exists for this library member
+        valid_membership = frappe.db.exists(
+            "Library Membership",
+            {
+                "library_member": self.library_member,
+                "docstatus": DocStatus.submitted(),
+                "from_date": ("<=", self.transaction_date),
+                "to_date": (">=", self.transaction_date),
+            },
+        )
+        if not valid_membership:
+            frappe.throw("The member does not have a valid membership")
